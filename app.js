c306aee29cadede3e9b5f7d48ec2d42826075849
Sunil <sunilchaudhary3112@gmail.com>
2017-06-10 10:40:34 +0530

    Added swagger to test service for api documentation

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+var swaggerJSDoc = require('swagger-jsdoc');
 const express = require('express');
+var path = require('path');
 const app = express();
 const greeting_route = require('./routes/get_greeting');
 
+// swagger definition
+var swaggerDefinition = {
+  info: {
+    title: 'Node Swagger API',
+    version: '1.0.0',
+    description: 'Demonstrating how to describe a RESTful API with Swagger',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+};
+
+// options for the swagger docs
+var options = {
+  // import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./routes/*.js'],
+};
+
+// initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/test', greeting_route);
-// app.get('/', (req, res)=>{
-//     res.send('Hello World');
-// });
+app.use('/api/test', greeting_route);
+
+// serve swagger
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 app.listen(3000, ()=> console.log('Server running at port 3000'));
--- routes/get_greeting.js
+++ routes/get_greeting.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',
+/**
+ * @swagger
+ * definition:
+ *   Greeting:
+ *     properties:
+ *       name:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /api/test/{name}:
+ *   get:
+ *     tags:
+ *       - test
+ *     description: Returns greetings message
+ *     produces:
+ *       - application/json
+*     parameters:
+ *       - name: name
+ *         description: Your name
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Server working fine
+ *         schema:
+ *           $ref: '#/definitions/Greeting'
+ */
+router.get('/:name',
     function (req, res) {
-        res.send('Welcome to express app');
+        var name = req.params.name
+        res.send("Welcome to express app " + name);
     });
 
 module.exports = router;
-
-//http://mherman.org/blog/2016/05/26/swagger-and-nodejs/#.WTlf2ROGO5x
